63500d6e03be19645863c43e8e1fb1d552b21efa
Yosra Chebbi <yosrabouzouita16@gmail.com>
2021-10-13 00:13:57 +0200

    Insérer les produits dans la page d’accueil manière2

--- front/js/script.js
+++ front/js/script.js
@@ -20,14 +20,36 @@ async function addProductsToHtml()
     let html='';
     for (let i = 0; i < res.length; i++) 
     {
-        html+= `<a href="./product.html?id=${res[i]._id}">
+       html+= `<a href="./product.html?id=${res[i]._id}">
         <article>
           <img src="${res[i].imageUrl}" alt="${res[i].altTxt}">
           <h3 class="productName">${res[i].name}</h3>
           <p class="productDescription">${res[i].description}</p>
         </article>
       </a>`
+      /* 
+        let a = document.createElement('a');
+        a.href = "./product.html?id=${res[i]._id}";
+        elem.appendChild(a);
         
+        let article = document.createElement('article');
+        a.appendChild(article);
+        
+        let image = document.createElement('img');
+        image.src = res[i].imageUrl;
+        image.alt = res[i].altTxt;
+        article.appendChild(image);
+        
+        let h3 = document.createElement('h3');
+        h3.class = "productName";
+        h3.innerText = res[i].name;
+        article.appendChild(h3);  
+        
+        let p = document.createElement('p');
+        p.class = "productDescription";
+        p.innerText = res[i].description;
+        article.appendChild(p);
+        */
      }
 
      elem.innerHTML=html;
